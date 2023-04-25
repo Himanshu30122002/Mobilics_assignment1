88f368d0ae00a6b494fbc1ee2efe66507e222792
Himanshu30122002 <83854035+Himanshu30122002@users.noreply.github.com>
2023-04-26 03:26:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,17 +7,7 @@ const connectDB = require("./db/connect");
 
 const products_routes = require("./routes/products");
 
-if (process.env.NODE_ENV === "production") {
-    const path = require("path");
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
-            if(err) {
-                res.status(500).send(err)
-            }
-        });
-    })
-}
+
 
 //middleware or set routes
 app.use("/api/products", products_routes);
@@ -35,6 +25,18 @@ const start = async () => {
 };
 
 start()
+
+if (process.env.NODE_ENV === "production") {
+    const path = require("path");
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        });
+    })
+}
 
 app.get("/", (req, res) => {
     
